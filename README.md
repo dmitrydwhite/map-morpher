82edd547affadfb4aaed3b25badea8bb90b4dd58
Dmitry White <dmitry.white@nike.com>
2016-08-29 17:18:05 -0700

    Update to web app

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-# Fantasy map generator
+# Dmitry's Fantasy Map Tool
 
-This is code for generating fantasy maps, using the algorithm behind [@unchartedatlas][uncharted]. For more details, see [these notes][notes].
+## Dev Notes
+### Notes on UI
+* Small, Medium, or Large Map
+* Add random terrain elements
+* Erode the map
+* Determine number of central cities/territories and number of smaller cities
+* Generate the map
+* Present a way to update labels
+
+## Attribution
+This is a web interface for the amazing fantasy map generating algorithms by [mewo2][mewo2], who I have never met but can only assume is a genius.  I am using his code found [here][githubTerrain] as the underlying engine for my interface.
+
+He uses this code for a Twitter Bot that creates a new fantasy map every hour; find it at [@unchartedatlas][uncharted]. For his detailed description of his process, see [his page on generating terrain][notes].
 
 [uncharted]: https://twitter.com/unchartedatlas
 [notes]: http://mewo2.com/notes/terrain/
+[mewo2]: https://github.com/mewo2
+[githubTerrain]: https://github.com/mewo2/terrain
---
+++ controls.js
@@ -0,0 +1,150 @@
+var WorkingMap;
+var totalCities = 0;
+
+var d3svg = d3.select('svg.map')
+  .attr("height", 800)
+  .attr("width", 800)
+  .attr("viewBox", "-500 -500 1000 1000");
+
+var mapSizes = {
+  small: 4096,
+  medium: 8192,
+  large: 16384
+};
+
+var terrainOperations = {
+  slope : function () {
+    WorkingMap = add(WorkingMap, slope(WorkingMap.mesh, randomVector(4)));
+  },
+  cone : function () {
+    WorkingMap = add(WorkingMap, cone(WorkingMap.mesh, -0.5));
+  },
+  invert: function () {
+    WorkingMap = add(WorkingMap, cone(WorkingMap.mesh, 0.5));
+  },
+  blobs : function () {
+    var blobs = $('input.terr-blob-num').val();
+
+    blobs = blobs < 25 && blobs > 0 ? blobs : 5;
+    WorkingMap = add(WorkingMap, mountains(WorkingMap.mesh, blobs));
+  },
+  norm : function () {
+    WorkingMap = normalize(WorkingMap);
+  },
+  round : function () {
+    WorkingMap = peaky(WorkingMap);
+  },
+  relax : function () {
+    WorkingMap = relax(WorkingMap);
+  },
+  seas : function () {
+    WorkingMap = setSeaLevel(WorkingMap, 0.5);
+  }
+};
+
+var landscapeAges = {
+  young: {
+    erosions: 1,
+    cleans: 1
+  },
+  medium: {
+    erosions: 2,
+    cleans: 1
+  },
+  old: {
+    erosions: 3,
+    cleans: 2
+  },
+  ancient: {
+    erosions: 5,
+    cleans: 3
+  }
+};
+
+var drawWorkingMap = function () {
+  visualizeVoronoi(d3svg, WorkingMap, -1, 1);
+  drawPaths(d3svg, 'coast', contour(WorkingMap, 0));
+}
+
+var drawLineMap = function () {
+  PoliticalMap.terr = getTerritories(PoliticalMap);
+  drawPaths(d3svg, 'coast', contour(PoliticalMap.h, 0));
+  drawPaths(d3svg, 'river', getRivers(PoliticalMap.h, 0.01));
+  drawPaths(d3svg, 'border', getBorders(PoliticalMap));
+  visualizeSlopes(d3svg, PoliticalMap);
+  visualizeCities(d3svg, PoliticalMap);
+};
+
+var drawMapOfSize = function (size) {
+  WorkingMap = zero(generateGoodMesh(mapSizes[size]));
+  drawWorkingMap();
+};
+
+$('.terrain-controls').find('button, input').not('[data-generate=true]').prop('disabled', true);
+$('.political-controls').find('button, input').prop('disabled', true);
+
+$('.create-map').find('button').on('click', function (evt) {
+  $('svg.map').empty();
+  var $btn = $(evt.target);
+  var size = $btn.data('size');
+
+  drawMapOfSize(size);
+  $('.terrain-controls').find('button, input').prop('disabled', false);
+});
+
+$('.make-terrain').find('button').on('click', function (evt) {
+  var $btn = $(evt.target);
+  var operation = $btn.data('operation');
+
+  terrainOperations[operation]();
+  drawWorkingMap();
+});
+
+$('.erode-terrain').find('button').on('click', function (evt) {
+  var $btn = $(evt.target);
+  var age = $btn.data('terrainAge');
+  var iters = landscapeAges[age];
+
+  // Erode the number of times for the age
+  for (var i=0; i<=iters.erosions; i++) {
+    WorkingMap = doErosion(WorkingMap, 0.1);
+  }
+  
+  // Set sea level to median
+  WorkingMap = setSeaLevel(WorkingMap, 0.5);
+
+  // Clean up the coastlines appropriately
+  for (var j=0; j<=iters.cleans; j++) {
+    WorkingMap = cleanCoast(WorkingMap, 1);
+    WorkingMap = fillSinks(WorkingMap);
+  }
+
+  drawWorkingMap();
+});
+
+$('.terrain-controls').find('button.finalize-terrain').on('click', function () {
+
+  $('.terrain-controls').find('button, input').not('[data-generate=true]').prop('disabled', true);
+  $('.political-controls').find('button, input').prop('disabled', false);
+
+  PoliticalMap = {
+    h: WorkingMap,
+    params: defaultParams,
+    cities: []
+  };
+
+  $('svg.map').empty();
+  drawLineMap();
+});
+
+$('.political-controls').find('button.civic-make').on('click', function () {
+  var majors = $('input[name=major-cities]').val();
+  var minors = $('input[name=minor-cities]').val();
+
+  PoliticalMap.params.nterrs = majors;
+  PoliticalMap.params.ncities = minors;
+
+  placeCities(PoliticalMap);
+
+  drawLineMap();
+})
